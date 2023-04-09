f8de7c047c7fc5bb26b343e26b63b10f0a0e0e05
Snehal Senapati <omsenapati23@gmail.com>
2023-04-09 20:36:45 +0530

    added

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import CovidData from './CovidData';
+
+function App() {
+  return (
+    <div className="App">
+     <CovidData />
+    </div>
+  );
+}
+
+export default App;
